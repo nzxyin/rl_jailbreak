dda00f5069f6488217cd7a97ee71a850a75c89ba
= <nzxyin@berkeley.edu>
2023-12-07 03:44:38 +0000

    added sft.py

--- sft.py
+++ sft.py
@@ -16,22 +16,20 @@ def main(args):
     if not os.path.exists(args.save_dir):
         os.mkdir(args.save_dir)
 
-    
-
     df = pd.read_csv(args.dataset)    
-    # rename the column "question" to "query"
-    df = df.rename(columns={"question": "query"})
+    # rename the column "prompt" to "text"
+    df = df.rename(columns={"prompt": "text"})
     
-    # remove the other columns risk_area,types_of_harm,source,new_category
-    df = df.drop(columns=["Unnamed: 0", "risk_area", "types_of_harm", "source", "new_category"])
+    # remove the other columns 
+    df = df.drop(columns=["platform", "source", "jailbreak", "created_at", "date", "community_id", "community_name"])
 
     dataset = Dataset.from_pandas(df)
     # TODO encode dataset using target model and slice <BOS> token off
 
     peft_config = LoraConfig(
-        r=16,
-        lora_alpha=32,
-        lora_dropout=0.05,
+        r=args.lora_r,
+        lora_alpha=args.lora_alpha,
+        lora_dropout=args.lora_dropout,
         bias="none",
         task_type="CAUSAL_LM",
     )
@@ -43,80 +41,20 @@ def main(args):
         train_dataset=dataset,
         dataset_text_field="text", # TODO: header name here
         peft_config=peft_config, 
-        max_seq_length=512,
+        max_seq_length=args.sft_max_seq_len,
+        output_dir=args.save_dir,
+        logging_strategy='epochs',
+        save_strategy='epochs',
+        evaluation_strategy='epochs',
+        save_total_limit=1,
+        warmup_ratio=0.1,
+        per_device_train_batch_size=args.sft_batch_size,
+        per_device_eval_batch_size=args.sft_batch_size,
+        load_best_model_at_end=True,
+        num_train_epochs=args.sft_num_epochs,
     )
 
-    target_kwargs = {
-
-    }
-
-    ppo_trainer = PPOTrainer(
-        model=generator.model,
-        config=ppo_config,
-        dataset=dataset,
-        tokenizer=generator.tokenizer,
-    )
-    device = ppo_trainer.accelerator.device
-
-    # TODO: setup W&B logging
-
-
-    # TODO update training loop
-    for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
-        if epoch >= args.ppo_num_epochs:
-            break
-
-        print(f"Epoch {epoch}")
-
-        # TODO: change back to list[Tensor]
-        MAX_LENGTH = args.ppo_
-        generator_input_tokens = [ppo_trainer.tokenizer("the", return_tensors='pt', padding='max_length', truncation=True, max_length=MAX_LENGTH).input_ids.to(device).squeeze()] * ppo_config.batch_size
-        print(generator_input_tokens)
-        # print(ppo_trainer.generate(ppo_trainer.tokenizer.encode("the", return_tensors='pt')[0].to(device)))
-        # TODO: change back to list[Tensor]
-        # ppo_trainer.model.generate(i, **generator_kwargs)
-        generator_output_tensors = [ppo_trainer.model.generate(generator_input_tokens[0].unsqueeze(0), **generator_kwargs).squeeze()[-MAX_LENGTH:] for i in generator_input_tokens]
-        
-        batch["attack"] = [ppo_trainer.tokenizer.batch_decode(i)[0] for i in generator_output_tensors]
-        # print(batch["attack"])
-        # print(batch["query"])
-        target_inputs = [" ".join([attack, query]) for attack, query in zip(batch["attack"], batch["query"])]
-        print(target_inputs)
-
-        # TODO: convert target into pipeline object?
-        target_outputs = [target.generate(i) for i in target_inputs]
-        print(target_outputs)
-
-        #### Compute reward score
-        # TODO: check type and shape of output. HIGH PRIO
-        # TODO: convert reward into pipeline object? LOW PRIO
-        
-        # TODO: return list of tensors
-        rewards = [reward_model.generate(i) for i in target_outputs]
-        print(rewards)
-
-        # TODO: Add diversity metrics here
-
-        #### Run PPO step
-        # TODO: DEBUG THIS incorrect type
-        stats = ppo_trainer.step(generator_input_tokens, generator_output_tensors, rewards)
-        ppo_trainer.log_stats(stats, batch, rewards)
-  
-        #### Save model
-        # FIXME: @Xavier the following three lines.
-        """
-        Traceback (most recent call last):
-        File "/data1/charlieji/rl_jailbreak/main.py", line 170, in <module>
-            main(args)
-        File "/data1/charlieji/rl_jailbreak/main.py", line 94, in main
-            if not os.path.exists(args.save_dir):
-        AttributeError: 'Namespace' object has no attribute 'save_dir'
-        """
-        # 
-        ppo_trainer.save_model(args.save_dir)
-        # generator.model.push_to_hub("my-fine-tuned-model-ppo")
-        # generator.model.save_pretrained("my-fine-tuned-model-ppo") # source: https://huggingface.co/docs/trl/quickstart, only the most recent one got saved. FIXME
-
+    trainer.train()
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
@@ -127,12 +65,6 @@ if __name__=="__main__":
         default = "gpt2-medium",
         help = "Name of attack generator model.",
         choices=["gpt2-medium"]
-    )
-    parser.add_argument(
-        "--generator-max-tokens",
-        type = int,
-        default = 32,
-        help = "Maximum number of generated tokens for the attacker."
     )
     
     ##################################################
@@ -147,26 +79,26 @@ if __name__=="__main__":
     
     ##################################################
 
-    ########### PPO parameters ##########
+    ########### LoRA parameters ##########
     parser.add_argument(
-        "--ppo-lr",
-        default = 1.5e-5,
-        help = "Learning rate for PPO",
+        "--lora-r",
+        default = 16,
+        help = "Rank of update matrix for LoRA",
+        type = int,
+    )
+
+    parser.add_argument(
+        "--lora-alpha",
+        default = 32,
+        help = "Alpha value for LoRA",
         type = float,
     )
 
     parser.add_argument(
-        "--ppo-num-epochs",
-        default = 100,
-        help = "Number of epochs for PPO",
-        type = int,
-    )
-
-    parser.add_argument(
-        "--ppo-batch-size",
-        default = 64,
-        help = "Batch size for PPO",
-        type = int,
+        "--lora-dropout",
+        default = 0.05,
+        help = "Dropout for LoRA",
+        type = float,
     )
     
     # TODO: add other PPO params
@@ -176,9 +108,41 @@ if __name__=="__main__":
     
     parser.add_argument(
         "--save_dir",
-        default="./results",
+        default="./sft_results",
         help = "Path to save the model.",
         type = pathlib.Path,
+    )
+
+    ##################################################
+
+    ########### SFT parameters ##########
+    
+    parser.add_argument(
+        "--sft-max-seq-len",
+        default=512,
+        help = "Maximum sequence length for SFT",
+        type = int,
+    )
+
+    parser.add_argument(
+        "--sft-num_epochs",
+        default=10.,
+        help = "Num epochs for SFT",
+        type = float,
+    )
+
+    parser.add_argument(
+        "--sft-lr",
+        default=5e-5,
+        help = "Learning Rate for SFT",
+        type = float,
+    )
+
+    parser.add_argument(
+        "--sft-batch-size",
+        default=16,
+        help = "Batch Size for SFT",
+        type = int,
     )
 
     ##################################################
